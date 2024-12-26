261c03a768182789a2fe0f220b326460540940cd
Prasanna Patil <proplayer14102004@gmail.com>
2024-12-26 12:53:56 +0530

    Update main.dart

--- Swipe-Deck-Cards-In-Flutter-main/lib/main.dart
+++ Swipe-Deck-Cards-In-Flutter-main/lib/main.dart
@@ -165,6 +165,21 @@ class _TestPageState extends State<TestPage> with TickerProviderStateMixin {
 
 
 const IMAGES = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
   "apex",
   "cod",
   "destiny",
